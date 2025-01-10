d2374fc4a2fb95b3b62da5de6e186e0313c37086
WALLE1Doctor1Who <msteier2.0@gmail.com>
2025-01-10 00:44:05 -0600

    Added row and column constants
    I've added constants for the number of rows and columns for the keypad, along with the pins for the rows and columns.

--- F-Key_Keyboard.ino
+++ F-Key_Keyboard.ino
@@ -7,6 +7,14 @@
 #include <Keypad.h>
 #include <Keyboard.h>
 
+// The number of rows in the keyboard matrix
+const byte ROW_COUNT = 3;
+// The number of columns in the keyboard matrix
+const byte COLUMN_COUNT = 4;
+// The pins responsible for the rows in the keyboard matrix
+const byte[ROW_COUNT] ROW_PINS = {2, 3, 4};
+// The pins responsible for the columns in the keyboard matrix
+const byte[COLUMN_COUNT] COLUMN_PINS = {5, 6, 7, 8};
 void setup() {
     // put your setup code here, to run once:
 
