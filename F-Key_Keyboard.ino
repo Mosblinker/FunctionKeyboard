1cc69abfba3bb34e6615fda3a4a62933ee744e68
WALLE1Doctor1Who <msteier2.0@gmail.com>
2025-01-10 00:45:55 -0600

    Added keypad
    I've added a character map of the keyboard and a Keypad object to use to scan the keyboard matrix.

--- F-Key_Keyboard.ino
+++ F-Key_Keyboard.ino
@@ -17,6 +17,15 @@ const byte COLUMN_COUNT = 4;
 const byte[ROW_COUNT] ROW_PINS = {2, 3, 4};
 // The pins responsible for the columns in the keyboard matrix
 const byte[COLUMN_COUNT] COLUMN_PINS = {5, 6, 7, 8};
+// A character map to represent the keys of the keyboard. 
+const char[ROW_COUNT][COLUMN_COUNT] KEYBOARD_MAP = {
+    {KEY_F13, KEY_F14, KEY_F15, KEY_F16},
+    {KEY_F17, KEY_F18, KEY_F19, KEY_F20},
+    {KEY_F21, KEY_F22, KEY_F23, KEY_F24}
+};
+// The keypad that is being scanned
+Keypad keyPad = Keypad(makeKeymap(KEYBOARD_MAP), ROW_PINS, COLUMN_PINS, ROW_COUNT, COLUMN_COUNT);
+
 void setup() {
     // put your setup code here, to run once:
 
